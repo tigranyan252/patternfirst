6ffd3dde8193414e07dbb7d937c5ee2bd000213a
Serj Tigranyan <serj-tigranyan@mail.ru>
2023-12-07 18:32:09 +0200

    Добавьте файлы проекта.

---
+++ patternfirst/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public interface IBankAccount
+{
+    void Withdraw(decimal amount);
+    decimal GetBalance();
+}
+
+public class RealBankAccount : IBankAccount
+{
+    private decimal balance;
+
+    public RealBankAccount(decimal initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > balance)
+        {
+            Console.WriteLine("Недостаточно средств на счете");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine($"Снято {amount} со счета. Остаток: {balance}");
+        }
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+}
+
+public class BankCheckProxy : IBankAccount
+{
+    private RealBankAccount realAccount;
+
+    public BankCheckProxy(decimal initialBalance)
+    {
+        realAccount = new RealBankAccount(initialBalance);
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        realAccount.Withdraw(amount);
+    }
+
+    public decimal GetBalance()
+    {
+        return realAccount.GetBalance();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IBankAccount bankAccount = new BankCheckProxy(1000);
+
+        bankAccount.Withdraw(500);
+        decimal balance = bankAccount.GetBalance();
+        Console.WriteLine($"Остаток на счете: {balance}");
+    }
+}
